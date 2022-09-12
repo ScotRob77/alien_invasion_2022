c00fb8d923813500ae44697af68563205dea35c2
ScotRob77 <rob.wilkes@yahoo.co.uk>
2022-09-12 08:59:54 +0000

    Add rules for game

--- run.py
+++ run.py
@@ -1,1 +1,33 @@
 import random
+
+
+def rules():
+    """
+    Rules of the game.
+    """
+    print("        WELCOME TO ALIEN INVASION...!\n")
+    print("Hello Earhling, we are from the plant Alshabaa...\n")
+    print("We have been watching Earth for many years...\n")
+    print("It seems humans have ruined your planet...\n")
+    print("So we are here to take over...!\n")
+    print("If you want to stop us from invading...\n")
+    print("You need to guess which country we intend to invade first.\n")
+    print("But beware... You can only guess wrong 6 times...\n")
+    welcome()
+
+
+def intro():
+
+
+def welcome():
+
+
+def play_again():
+
+
+def create_random_country():
+
+
+def game_start():
+
+
